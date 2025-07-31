47326a7463e50c582b9d70a7d8fa33a3455f8636
Muhammad Gulzareen Hassan <gul92749@gmail.com>
2025-07-31 23:13:53 +0500

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,12 @@
+#--------Task2  PartA-----
+A=input("Enter your name")
+B=input("Your Age?")
+C=input("Your profession")
+D=input("Your address")
+print(f"So your name is {A},you are {B} years old and you are a {C} by profession.You live in {D},Thanks for the information {A}!")
+
+#--------Task2  PartB-----
+A=3
+B=4
+A,B=B,A
+print(A,B)  #This line will just swap A and B
