cddb3c4827c708dbc56a43f9296a76e33d93cea4
Muhammad Gulzareen Hassan <gul92749@gmail.com>
2025-07-31 23:11:22 +0500

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,21 @@
+#Fixing badly Indented
+
+#-------A badly indented code that will give some errors------
+  #name = input("Enter your name: ")
+#if name == "Gulzareen":
+#print("Hello Gulzareen!")
+ #print("Welcome to Python basics.")
+#else:
+#print("Hi there!")
+  #print("Let's start learning Python.")
+
+#----Lets fix this-----
+name = input("Enter your name: ")
+if name == "Gulzareen":
+    print("Hello Gulzareen!")    #here was the first error that was corrected with 4 spaces
+    print("Welcome to Python basics.")#same
+else:
+    print("Hi there!")#//
+    print("Let's start learning Python.")#//
+#Now i will run perfectly without any errors
+
